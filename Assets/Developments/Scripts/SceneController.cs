f360dcb2c5453a148b8faedfa5cbdae069a98840
MiraOka <uaofmsbct.9g@gmail.com>
2018-10-31 17:03:13 +0900

    move scene

---
+++ Assets/Developments/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+
+	void Start () {
+        InteractionManager.InteractionSourcePressed += tapHomebase;
+	}
+
+    void tapHomebase(InteractionSourcePressedEventArgs e)
+    {
+        SceneManager.LoadScene("Main");
+        InteractionManager.InteractionSourcePressed -= tapHomebase;
+    }
+}
